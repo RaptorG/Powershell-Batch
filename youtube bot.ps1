d2dfe5a447eb3f5a29102c906fa02758c0b335cf
karatyman <vincentmorris76@gmail.com>
2015-05-23 14:25:34 -0400

    Youtube bot
    Watched a youtube video automatically

---
+++ youtube bot.ps1
@@ -0,0 +1,16 @@
+﻿$Link = 'https://www.youtube.com/watch?v=e-ORhEE9VVg' #Blank Space By Taylor Swift
+$loop = 0
+
+While ($loop -eq 0)
+{
+   for($i=1
+     $i -le 10 #change this 10 to the number of windows you want to open
+     $i++)
+     {
+       &("C:\Program Files (x86)\Google\Chrome\Application\chrome.exe") /incognito $Link
+     }
+    timeout 300 # this waits 5 minutes by default to let the video play
+    
+    Get-Process | Where Name –Like “chrome*” | Stop-Process 
+    # ends the chrome process to do it all again!!!
+    }
