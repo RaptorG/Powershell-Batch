a8553f04c05dd5ea1344f63d7bd72768c26f1af3
karatyman <vincentmorris76@gmail.com>
2015-06-30 22:38:00 -0400

    Spam Bot
    This is a basic script that allows you to send a word or phrase a
    repeated number of times.

---
+++ Spambot.ps1
@@ -0,0 +1,26 @@
+﻿function sendkey{
+
+$wshell = New-Object -ComObject wscript.shell;
+$wshell.AppActivate('title of the application window')
+$wshell.SendKeys($Spam)
+$wshell.SendKeys('~')
+
+}
+
+"Welcome to Vinny's Spammer"
+'Please type the words or phrase you want to be spammed'
+
+$Spam = Read-host
+    'Now Select the number of times to spam this'
+        $Multiplier = Read-Host
+    'finally, how many seconds do you want to delay each message (.5 recommend, 0 Causes issues)'
+        $delay = Read-Host
+'spamming will commence in 5 seconds'
+sleep 2
+$i = 0
+while ($i -le $Multiplier)
+{
+    sleep $delay
+    sendkey
+    $i = $i + 1
+}
